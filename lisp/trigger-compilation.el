e33427365b2d2fda56fd857551b5e0bfd4e80cf8
Hugo Sales <hugo@hsal.es>
2023-07-30 21:32:01 +0100

    Fix waiting for native compilation and use -O3 and trampolines

--- lisp/trigger-compilation.el
+++ lisp/trigger-compilation.el
@@ -2,20 +2,27 @@
 
 ;;; Code:
 
+(require 'comp)
+
 (setq native-comp-jit-compilation t
       native-comp-async-report-warnings-errors nil
-      inhibit-message nil)
+      native-comp-speed 3 ;; -O3
+      native-comp-enable-subr-trampolines t
+      native-comp-jit-compilation-deny-list (append native-comp-jit-compilation-deny-list ".*/tests?/.*")
+      inhibit-message nil
+      )
 
+;; Use async version as it can recurse
 (native-compile-async user-emacs-directory 'recursively nil
                       (lambda (file)
                         (let ((should (not (string-match-p "/tests?/" file))))
-                          (if should (message "Compiling %s" file)
+                          (if should (message "Will compile %s" file)
                             (when (getenv "DEBUG") (message "Ignoring %s" file)))
                           should)))
 
-(while (> (hash-table-count comp-deferred-pending-h) 0)
-  (message "Waiting for compilation to finish...")
-  (sit-for 1))
+;; but block until native compilation has finished
+(while (or comp-files-queue (> (comp-async-runnings) 0))
+  (sleep-for 1))
 
 (message "Compilation finished!")
 
