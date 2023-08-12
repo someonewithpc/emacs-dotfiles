42418fd43c97a3ec12c500834ca1a43dd6bb6da4
Hugo Sales <hugo@hsal.es>
2023-08-12 18:30:49 +0100

    Add some basic keybinds

---
+++ lisp/keybinds.el
@@ -0,0 +1,10 @@
+(set-register ?e `(file . ,(locate-user-emacs-file "init.el")))
+
+(put 'scroll-left 'disabled nil)
+(put 'scroll-right 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+
+
+	      
