5f1ce92ef8b886f59a46eada9b166f416e4392f6
Hugo Sales <hugo@hsal.es>
2023-08-13 13:28:29 +0100

    Bind scroll keys

--- lisp/keybinds.el
+++ lisp/keybinds.el
@@ -5,5 +5,8 @@
 (put 'narrow-to-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
+(global-set-key (kbd "<M-up>") 'scroll-up-line)
+(global-set-key (kbd "<M-down>") 'scroll-down-line)
+
 (put 'overwrite-mode 'disabled "Overwrite mode is disabled becuase it's crap.")
 (global-set-key [remap overwrite-mode] 'ignore)
