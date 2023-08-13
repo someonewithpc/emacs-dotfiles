e741f8419c43d5ac3ace9e9e10236cdb9ff69869
Hugo Sales <hugo@hsal.es>
2023-08-13 13:28:41 +0100

    Fix hexl

--- lisp/packages/editing.el
+++ lisp/packages/editing.el
@@ -18,16 +18,14 @@
   )
 
 (use-package hexl
-  :ensure t
   :commands (hexl-mode)
   :hook
   (find-file . (lambda ()
 		 "Use hexl-mode if opening a binary file (containing NUL bytes)"
 		 (unless (eq major-mode 'hexl-mode)
-		   (when (with-current-buffer (or buffer (current-buffer))
-			   (save-excursion
-			     (goto-char (point-min))
-			     (search-forward (string ?\x00) nil t 1)))
+		   (when (save-excursion
+			   (goto-char (point-min))
+			   (search-forward (string ?\x00) nil t 1))
 		     (hexl-mode)))
 		 ))
   )
