7cab52f5f453f231d3839f30d7c19c5eed17b542
Hugo Sales <hugo@hsal.es>
2023-08-13 12:10:42 +0100

    Remove visual bell

--- lisp/theme.el
+++ lisp/theme.el
@@ -5,8 +5,5 @@
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-material-dark t)
 
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
