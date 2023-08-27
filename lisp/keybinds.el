55ddbb26b1decbeb5d3128240803ed91ef9b57fd
Hugo Sales <hugo@hsal.es>
2023-08-27 22:16:59 +0100

    Add logical beginning and end of line move functions

--- lisp/keybinds.el
+++ lisp/keybinds.el
@@ -10,3 +10,23 @@
 
 (put 'overwrite-mode 'disabled "Overwrite mode is disabled becuase it's crap.")
 (global-set-key [remap overwrite-mode] 'ignore)
+
+(defun move-beginning-of-line-logical ()
+  (interactive)
+  (if (not (bolp))
+      (move-beginning-of-line nil)
+    (back-to-indentation)))
+
+(defun move-end-of-line-logical ()
+  (interactive)
+  (if (not (eolp))
+      (move-end-of-line nil)
+    (let ((comment-pos (save-excursion
+                         (move-end-of-line nil)
+                         (comment-search-backward (line-beginning-position) t)))) ;; adasd
+      (if comment-pos
+          (goto-char (1- comment-pos))
+        (move-end-of-line nil)))))
+
+(global-set-key [remap move-beginning-of-line] #'move-beginning-of-line-logical)
+(global-set-key [remap move-end-of-line] #'move-end-of-line-logical)
