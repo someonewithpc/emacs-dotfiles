84b892c9d141bee7b87248407b775859366f5639
Hugo Sales <hugo@hsal.es>
2023-09-02 16:35:04 +0100

    Trigger compilation as last step, so it can depend on loaded files

--- init.el
+++ init.el
@@ -2,8 +2,11 @@
 
 ;; (load "/home/hugo/projects/emacs-dotfiles/local/eln-cache/30.0.50-fba4a546/utils-ac515563-8f9b87fe.eln")
 
-(dolist (file `(utils defaults automode straight theme packages ,@(when noninteractive '(trigger-compilation)) keybinds benchmark))
+(dolist (file '(utils defaults automode straight theme packages keybinds benchmark))
   (load (expand-file-name (file-name-concat "lisp/" (symbol-name file)) user-emacs-directory) nil (not init-file-debug) nil 'must-suffix)
   )
+
+(when noninteractive
+  (load (expand-file-name "lisp/trigger-compilation" user-emacs-directory) nil (not init-file-debug) nil 'must-suffix))
 
 ;;; init.el ends here
